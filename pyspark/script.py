b1dc0f219476fbe7b6975f78e3d389e4d9c8d9fc
SaiKumar0236 <167432776+SaiKumar0236@users.noreply.github.com>
2025-06-19 16:11:45 +0530

    Delete pyspark directory

--- pyspark/script.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
- spark = SparkSession.builder.appName("GitHubActionsTest").getOrCreate()
- data = [("Sai", 1), ("Kumar", 2)]
- df = spark.createDataFrame(data, ["name", "id"])
- df.show()
- spark.stop()
